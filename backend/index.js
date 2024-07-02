b5ae4a46ac3d061d850564d9db27331c308b341c
Gabriel Carriel <ga123carriel2015@gmail.com>
2024-07-02 09:44:13 -0300

    creating basic structure and the bff

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,20 @@
-// src/index.js
 import express from 'express';
-import session from 'express-session'
 import axios from 'axios';
+import cors from "cors"
 import 'dotenv/config'
 
 const app = express();
+app.use(cors())
 
-// Configuração de sessão
-app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-}));
-
-// Endpoint para busca no YouTube
 app.get('/api/search', async (req, res) => {
     const query = req.query.q;
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+    const response = await axios.get("https://www.googleapis.com/youtube/v3/search", {
         params: {
-        part: 'snippet',
-        q: query,
-        key: process.env.YOUTUBE_API_KEY,
+            part: "snippet",
+            type: "video",
+            maxResults: 16,
+            q: query,
+            key: process.env.YOUTUBE_API_KEY,
         },
     });
     res.json(response.data);
